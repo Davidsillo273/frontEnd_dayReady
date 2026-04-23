36adf23c182cde9d1e3ff6c37ad77e132c9a4be6
David <2017lasvegasedu@gmail.com>
2026-04-22 21:27:55 -0600

    Subiendo carpetas

---
+++ dayReadyWeb/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginAdmin from './Pages/admin/loginAdmin';
+import RegisterUser from './Pages/admin/registerUser';
+import RecoveryPass from './Pages/admin/recoveryPass';
+import LoginCustomer from './Pages/customer/loginCustomer';
+import RecoveryPassCustomer from './Pages/customer/recoveryPass';
+import RegisterCustomer from './Pages/customer/registerCustomer';
+
+function App() {
+
+  console.log("App se está cargando...");
+
+  return (
+    <Router>
+      <Routes>
+        {/* RUTAS DE CLIENTE */}
+        <Route path="/" element={<LoginCustomer />} />
+        <Route path="/customer/register" element={<RegisterCustomer />} />
+        <Route path="/customer/recovery" element={<RecoveryPassCustomer />} />
+
+        {/* RUTAS DE ADMIN */}
+        <Route path="/admin" element={<LoginAdmin />} />
+        <Route path="/admin/register" element={<RegisterUser />} />
+        <Route path="/admin/recovery" element={<RecoveryPass />} />
+
+        {/* 404 - Redirigir al inicio si la ruta no existe */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
